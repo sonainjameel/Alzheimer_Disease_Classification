d572e8d87644c8408c29685f0fbe0d76a4f87993
Sonain Jamil <90409208+sonainjameel@users.noreply.github.com>
2024-04-26 23:59:00 +0200

    Initial commit of webpage

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Document loaded, JavaScript is active!');
+    // More interactive JavaScript code can be added here
+});
